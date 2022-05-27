3bf553f938f88a7f4f774fe936e29a65289c887c
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2022-05-27 15:55:32 +0600

    3. @Autowired. Внедрение зависимости [#261276]

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,16 +1,16 @@
 package ru.job4j.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartUI {
-    private Store store;
-    private ConsoleInput consoleInput;
 
-    public StartUI(Store store, ConsoleInput consoleInput) {
-        this.store = store;
-        this.consoleInput = consoleInput;
-    }
+    @Autowired
+    private Store store;
+
+    @Autowired
+    private ConsoleInput consoleInput;
 
     public void add(String value) {
         store.add(value);
